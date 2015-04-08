bc1af6603737fd14885bb5ecf5498e24f1f1c0ee
unknown <jpogeant@EMF027D.internal.double-expresso.local>
2015-04-08 15:33:37 +0200

    [ADDED] Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    gulpLoadPlugins = require('gulp-load-plugins'),
+    plugins = gulpLoadPlugins();
+
+var app = 'app/Resources';
+
+var onError = function(err) {
+    console.log(err);
+};
+
+gulp.task('Sass', function() {
+
+    gulp.src(app + '/scss/**/*.scss')
+        .pipe(plugins.plumber({
+            errorHandler: onError
+        }))
+        .pipe(plugins.rubySass({
+            compass: true,
+            style: 'compressed',
+            check: true}))
+        .pipe(plugins.minifyCss({keepSpecialComments:0}))
+        .pipe(plugins.rename({suffix: '.min'}))
+        .pipe(gulp.dest('web/css/'));
+});
+
+
+
+
+
+
+
+gulp.task('watch', function() {
+    gulp.watch(app + '/scss/**/*.scss', ['Sass']);
+});
+
+
+gulp.task('build', [
+
+
+'Sass,'
+]);
+
+gulp.task('default', [
+'Sass,'
+]);
